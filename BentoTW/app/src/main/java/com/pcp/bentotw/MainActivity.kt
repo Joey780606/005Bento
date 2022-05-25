ee8a7d4141e73fbe8c88a563117fc2f98935e8c0
Joey <yty0606@gmail.com>
2022-05-25 14:35:52 +0800

    Feat: Create first welcome page.
    Solution:
    1. As title.
    2. Use Navigation, LaunchedEffect.
    
    Author: Joey

--- BentoTW/app/src/main/java/com/pcp/bentotw/MainActivity.kt
+++ BentoTW/app/src/main/java/com/pcp/bentotw/MainActivity.kt
@@ -3,26 +3,54 @@ package com.pcp.bentotw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.pcp.bentotw.ui.theme.BentoTWTheme
+import com.pcp.bentotw.ui.theme.Green0091A7
+import com.pcp.bentotw.ui.theme.Green00E6FE
+import kotlinx.coroutines.delay
 
+/*
+    Arthur: Joey yang
+    Use skill:
+    1. Navigation.
+    2. LaunchedEffect (Coroutine).
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BentoTWTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController() //Navigation Step2
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "initial_screen"
                 ) {
-                    Greeting("Android")
+                    composable("initial_screen") {
+                        //second screen
+                        InitialScreen01(navController = navController)
+                    }
+                    composable("login_screen") {
+                        //first screen
+                        LoginScreen02(navController = navController)
+                    }
+
                 }
             }
         }
@@ -30,14 +58,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun InitialScreen01(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Green00E6FE),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(id = R.drawable.logo),
+            contentDescription = "",
+        )
+        Text(text = stringResource(R.string.app_title),
+            color = Green0091A7)
+    }
+    LaunchedEffect(true) {
+        delay(2000)
+        navController.navigate("login_screen")
+    }
+}
+
+@Composable
+fun LoginScreen02(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Green00E6FE),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = stringResource(R.string.app_title),
+            color = Green0091A7)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BentoTWTheme {
-        Greeting("Android")
+
     }
 }
--- BentoTW/app/src/main/java/com/pcp/bentotw/ui/theme/Color.kt
+++ BentoTW/app/src/main/java/com/pcp/bentotw/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Green00E6FE = Color(0xFF00E6FE)
+val Green26C7DB = Color(0xFF26C7DB)
+val Green0091A7 = Color(0xFF0091A7)
+
+val Blue31B6FB = Color(0xFF31B6FB)
